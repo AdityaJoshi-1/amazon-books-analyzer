0be94449fdf4b12e37c78ba7ca279e23d82e7628
AdityaJoshi-1 <joshiaditya1007@gmail.com>
2026-09-19 16:58:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ def detect_subgenre(title):
 # ==========================================
 @st.cache_data
 def load_data():
-    df = pd.read_csv("data/amazon_books.csv")
+   df = pd.read_csv("amazon_books.csv")
     df["Sub-Genre"] = df["Name"].apply(detect_subgenre)
     return df
 
